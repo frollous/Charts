93adf67b46777e1f529312d609d8f81efa4402b2
Neil Cossor <neil.cossor@frollo.us>
2021-11-20 22:12:33 +1100

    WA-2743 update to Swift 5.0 syntax for SPM migration of app

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
@@ -11,5 +11,5 @@ let package = Package(
     targets: [
         .target(name: "Charts", dependencies: [])
     ],
-    swiftLanguageVersions: [4]
+    swiftLanguageVersions: [.v5]
 )
